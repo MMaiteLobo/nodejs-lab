aacc49de70270ce5462deb6d5347d09892d509dc
MMaiteLobo <maitemlobo@gmail.com>
2025-07-21 19:05:13 -0300

    ok hasta seccion 22

--- nodejs_MS/NODEJS-COMPLETE-GUIDE_9/controllers/auth.js
+++ nodejs_MS/NODEJS-COMPLETE-GUIDE_9/controllers/auth.js
@@ -10,7 +10,7 @@ const User = require('../models/user');
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      api_key: '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'
+      api_key: ''
     }
   })
 );
